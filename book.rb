d9df928d7ae37bc2c95864315c5bb3ce215f8c08
Aduda-Boaz <adudaboaz@gmail.com>
2022-07-05 19:48:53 +0300

    Add create book functionalities

--- book.rb
+++ book.rb
@@ -1,19 +1,28 @@
 require_relative 'item'
+require_relative 'label'
 require 'date'
 
 class Book < Item
   attr_accessor :publish_date, :publisher, :cover_state, :title
 
-  def initialize(title, publish_date)
+  def initialize(title, publish_date, publisher, cover_state, author, label)
     super(title, publish_date, archived: false)
     @publish_date = DateTime.parse(publish_date)
     @publisher = publisher
     @cover_state = cover_state
+    add_author(author)
+    @label = label
+    add_label(label)
   end
 
   def can_be_archived?
     return true if super || cover_state == 'bad'
 
     false
+  end
+
+  def to_hash
+    { title: @title, publish_date: @publish_date.strftime('%Y-%m-%d'), publisher: @publisher, cover_state: @cover_state,
+      author: @author.to_hash, label: @label.to_hash }
   end
 end
--- create_book.rb
+++ create_book.rb
@@ -1,8 +1,30 @@
 require_relative 'book'
+require_relative 'create_label'
 require_relative 'create_author'
 
 class CreateBook
   def initialize
+    @create_label = CreateLabel.new
     @create_author = CreateAuthor.new
+  end
+
+  def create_book
+    puts 'Enter book title:'
+    title = gets.chomp
+
+    author = @create_author.create_author
+
+    puts 'Enter publish date in order of YYYY-MM-DD:'
+    publish_date = gets.chomp
+
+    puts 'Enter name of publisher:'
+    publisher = gets.chomp
+
+    puts 'Enter book condition (bad, good, new):'
+    cover_state = gets.chomp
+
+    label = @create_label.create_label
+
+    Book.new(title, publish_date, publisher, cover_state, author, label)
   end
 end
