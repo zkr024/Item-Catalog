10da846f2d61f96a7e3aeb75e898cb1dc6ae3d05
zkr024 <zkr.bm.02403@gmail.com>
2022-07-05 16:00:42 -0500

    List functionality

--- app.rb
+++ app.rb
@@ -4,6 +4,9 @@ require './functions/record'
 require './functions/create_author'
 require './functions/create_book'
 require './functions/create_label'
+require './functions/book_list'
+require './functions/label_list'
+
 
 def app
   record = Record.new
@@ -13,10 +16,10 @@ def app
     load_menu
     if valid_input?(@input)
       case @input
-      when '1' then puts 'test 1'
+      when '1' then BookList.new.book_list(record.book)
       when '2' then puts 'test 1'
       when '3' then puts 'test 1'
-      when '4' then puts 'test 4'
+      when '4' then LabelList.new.label_list(record.label)
       when '5' then CreateBook.new.create_book(record.book, record.label, record.author)
       when '6' then puts 'test 1'
       when '7'
---
+++ functions/book_list.rb
@@ -0,0 +1,18 @@
+class BookList
+  def book_list(book)
+    if book.empty?
+      puts "Sorry, there're no books"
+    else
+      (book.map.with_index do |b, i|
+        puts "Book number #{i+1}"
+        puts "  -Title: #{b.title}"
+        puts "  -Author: #{b.author.first_name} #{b.author.last_name}"
+        puts "  -Type: #{b.label.title} Color-#{b.label.color}"
+        puts "  -Published on: #{b.publish_date}"
+        puts "  -Book status: #{b.cover_state}"
+        puts "  -Publisher: #{b.publisher}"
+        puts "\n"
+      end)
+    end
+  end
+end
