1e1c34e9254eb35d0e68dc2652c3d00d7d3597b6
Aduda-Boaz <adudaboaz@gmail.com>
2022-07-06 19:43:44 +0300

    Add Schema sql

---
+++ schema.sql
@@ -0,0 +1,47 @@
+CREATE TABLE Items
+(
+  id INT PRIMARY KEY,
+  publish_date DATE
+  archived BOOLEAN
+  genre_id INT
+  author_id INT
+  label_id INT
+  FOREIGN KEY (genre_id) REFERENCES Genre(id)
+  FOREIGN KEY (author_id) REFERENCES Author(id)
+  FOREIGN KEY (label_id) REFERENCES Label(id)
+);
+
+CREATE TABLE Genre
+(
+  id SERIAL PRIMARY KEY
+  name VARCHAR(100)
+);
+
+CREATE TABLE Author
+(
+  id SERIAL PRIMARY KEY
+  first_name VARCHAR(100)
+  last_name VARCHAR(100)
+);
+
+CREATE TABLE Label
+(
+  id SERIAL PRIMARY KEY
+  title VARCHAR(100)
+  color VARCHAR(100)
+);
+
+CREATE TABLE Book
+(
+  id SERIAL PRIMARY KEY
+  publisher VARCHAR(100)
+  cover_state VARCHAR(100)
+  FOREIGN KEY (id) REFERENCES Items(id)
+);
+
+CREATE TABLE MusicAlbum
+(
+  id SERIAL PRIMARY KEY
+  on_spotify BOOLEAN
+  FOREIGN KEY (id) REFERENCES Items(id)
+);
